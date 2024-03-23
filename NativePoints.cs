1bacf78be13c1a28870d58df71d55cae70845d7f
Aarnow <arnaudfgr.dev@gmail.com>
2024-03-23 12:00:40 +0100

    Initial Project Files

---
+++ NativePoints.cs
@@ -0,0 +1,27 @@
+﻿using Life;
+using ModKit.Helper;
+using ModKit.Interfaces;
+using ModKit.Internal;
+
+namespace NativePoints
+{
+    public class NativePoints : ModKit.ModKit
+    {
+
+        public NativePoints(IGameAPI api) : base(api)
+        {
+            PluginInformations = new PluginInformations(AssemblyHelper.GetName(), "1.0.0", "Aarnow");
+        }
+
+        public override void OnPluginInit()
+        {
+            base.OnPluginInit();
+            InitDirectoryAndFiles();
+            Logger.LogSuccess($"{PluginInformations.SourceName} v{PluginInformations.Version}", "initialisé");
+        }
+
+        private void InitDirectoryAndFiles()
+        {
+        }
+    }
+}
